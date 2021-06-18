950ada89140be1605a61ec28244ad9bc57361fc5
aearavindh <42218567+aearavindh@users.noreply.github.com>
2021-06-18 10:48:12 -0700

    All services added

---
+++ currency-exchange-service/src/main/java/com/aea/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,31 @@
+package com.aea.microservices.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	@GetMapping("/sample-api")
+//	@Retry(name = "sample-api", fallbackMethod="hardcodedResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod="hardcodedResponse")
+//	@RateLimiter(name = "default") //	RateLimiter 10s => 10000 calls to the sample-api
+	@Bulkhead(name = "default")
+	public String sampleApi() {
+		logger.info("Sample API call received");
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-api", String.class);
+//		return forEntity.getBody();
+		return "Sample API";
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-response";
+	}
+	
+}
